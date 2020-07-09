841854fbaba152a46066164ae1d863241656e2a5
Levi Mann <levimann@MacBook-Pro.attlocal.net>
2020-07-08 21:11:09 -0500

    Works to find shortest path without obsticals in straight line

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 import settings
 import grid
 import fill_block
+import open_set_block
+import math
 
 
 class AStar:
@@ -18,20 +20,44 @@ class AStar:
         self.grid_lines = []
         self._create_grid()
 
-        # initialize start
-        start_center = self._grid_location((4, 4))
-        self.start_location = fill_block.FillBlock(self, 'black', start_center)
-        end_center = (self.settings.WIDTH - self.settings.grid_width/2,
-                      self.settings.HEIGHT - self.settings.grid_height/2)
-        self.end_location = fill_block.FillBlock(self, 'red', end_center)
+        # initialize start and end
+        start_center = (4, 30)
+        end_center = (self.settings.WIDTH/self.settings.grid_width-2,
+                      self.settings.HEIGHT/self.settings.grid_height-2)
+        start_h = self._find_distance(start_center, end_center)
+        self.start_location = fill_block.FillBlock(
+            self, 'black', start_center, 0, start_h)
+        self.end_location = fill_block.FillBlock(
+            self, 'red', end_center, start_h, 0)
+        self.end_coords = self.end_location.coords
+
+        # initialize queue
+        self.queue = []
+        self.coords = []
+        self.short_path = []
+        self._create_queue(start_center, self.start_location.g)
 
     def on_event(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    sys.exit()
 
     def on_loop(self):
-        pass
+        if self.queue:
+            if self.short_path:
+                if self.short_path[-1].coords == self.end_coords:
+                    pass
+                else:
+                    self.check_lowest_f()
+                    self._create_queue(
+                        self.short_path[-1].coords, self.short_path[-1].g)
+            else:
+                self.check_lowest_f()
+                self._create_queue(
+                    self.short_path[-1].coords, self.short_path[-1].g)
 
     def on_render(self):
         self.screen.fill(self.settings.bg_color)
@@ -39,6 +65,10 @@ class AStar:
             line.draw_line()
         self.start_location.draw_fill()
         self.end_location.draw_fill()
+        for element in self.queue:
+            element.draw_open()
+        for element in self.short_path:
+            element.draw_fill()
         pygame.display.flip()
 
     def on_cleanup(self):
@@ -51,6 +81,39 @@ class AStar:
             self.on_loop()
             self.on_render()
         self.on_cleanup()
+# --------------------------------
+
+    def check_lowest_f(self):
+        lowest_f = None
+        lowest_i = None
+        for i in range(0, len(self.queue)):
+            current_f = self.queue[i].f
+            if i == 0:
+                lowest_f = current_f
+                lowest_i = i
+            elif current_f < lowest_f:
+                lowest_f = current_f
+                lowest_i = i
+        lowest_h = self.queue[lowest_i].h
+        lowest_g = self.queue[lowest_i].g
+        lowest_coords = self.queue[lowest_i].coords
+        self._create_short_path(lowest_coords, lowest_g, lowest_h)
+        del self.queue[lowest_i]
+
+    # def _find_path(self):
+    #     while self.queue:
+    #         if self.short_path:
+    #             if self.short_path[-1].coords == self.end_coords:
+    #                 break
+    #         else:
+    #             self.check_lowest_f()
+    #             self._create_queue(
+    #                 self.short_path[-1].coords, self.short_path[-1].g)
+    #             self.on_event()
+    #             # self.on_loop()
+    #             self.on_render()
+
+# ---------------------------------------------------------------------
 
     def _create_grid(self):
         # create x - axis lines
@@ -67,11 +130,57 @@ class AStar:
             line = grid.Grid(self, start, end)
             self.grid_lines.append(line)
 
-    def _grid_location(self, coords):
-        x = self.settings.grid_width/2 * (coords[0]*2+1)
-        y = self.settings.grid_height/2 * (coords[1]*2+1)
-        new = (x, y)
-        return new
+# ---------------------------------------------------------------------
+
+    def _find_distance(self, start, end):
+        x1 = start[0]
+        x2 = end[0]
+        y1 = start[1]
+        y2 = end[1]
+        d = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+        return d
+
+# -------------------------------------------------------------------
+    def _create_queue(self, node_coords, g):
+        x = node_coords[0]
+        y = node_coords[1]
+        x_r = x + 1
+        x_l = x - 1
+        y_u = y - 1
+        y_d = y + 1
+        one = (x_l, y_u)
+        two = (x, y_u)
+        three = (x_r, y_u)
+        four = (x_r, y)
+        five = (x_r, y_d)
+        six = (x, y_d)
+        seven = (x_l, y_d)
+        eight = (x_l, y)
+        coords = [one, two, three, four, five, six, seven, eight]
+        for node in coords:
+            if node[0] < 0 or node[0] > self.settings.WIDTH-1 or node[1] < 0 or node[1] > self.settings.HEIGHT:
+                continue
+            elif node in self.coords:
+                continue
+            else:
+                new = open_set_block.OpenBlock(
+                    self, 'blue', node, g, node_coords)
+                self.queue.append(new)
+                self.coords.append(node)
+
+    def _delete_queue(self, node):
+        self.queue.remove(node)
+
+    def _add_queue_coord(self, coord):
+        self.coords.append(coord)
+
+    def _del_queue_coord(self, coord):
+        self.coords.remove(coord)
+
+# ----------------------------------
+    def _create_short_path(self, coords, g, h):
+        new = fill_block.FillBlock(self, 'green', coords, g, h)
+        self.short_path.append(new)
 
 
 if __name__ == "__main__":
--- open_set_block.py
+++ open_set_block.py
@@ -1,13 +1,18 @@
 import pygame
+import math
 
 
-class FillBlock:
-    def __init__(self, main, color, coords, g, h):
+class OpenBlock:
+    def __init__(self, main, color, coords, prev_g, prev_coords):
         # initialize main settings
         self.screen = main.screen
         self.settings = main.settings
-        self.h = h
-        self.g = g
+        self.coords = coords
+        self.prev_coords = prev_coords
+        self.prev_g = prev_g
+        self.end_coords = main.end_coords
+        self.h = self._calculate_h()
+        self.g = self._calculate_g()
         self.f = self.g + self.h
 
         # initialize block
@@ -20,18 +25,33 @@ class FillBlock:
         self.color = color_map[color]
         self.height = self.settings.grid_height
         self.width = self.settings.grid_width
-        self.coords = coords
         self.center = self._grid_location(self.coords)
 
         # initialize fill
         self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = self.center
 
-    def draw_fill(self):
-        pygame.draw.rect(self.screen, self.color, self.rect)
+    def draw_open(self):
+        pygame.draw.rect(self.screen, self.color, self.rect, width=2)
 
     def _grid_location(self, coords):
         x = self.settings.grid_width/2 * (coords[0]*2+1)
         y = self.settings.grid_height/2 * (coords[1]*2+1)
         new = (x, y)
         return new
+
+    def _calculate_g(self):
+        x1 = self.coords[0]
+        x2 = self.prev_coords[0]
+        y1 = self.coords[1]
+        y2 = self.prev_coords[1]
+        d = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+        return d
+
+    def _calculate_h(self):
+        x1 = self.coords[0]
+        x2 = self.end_coords[0]
+        y1 = self.coords[1]
+        y2 = self.end_coords[1]
+        d = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+        return d
